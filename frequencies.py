1a5d6390c20669d8dce581ea6a63022f11e22d5d
Tsz Shun Max Chang <tsz.chang@kcl.ac.uk>
2022-09-30 08:43:56 +0100

    Add algorithm for frequencies program

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,9 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    s_items = [str(i) for i in items]
+
+    for i in s_items:
+        frequencies[i] = s_items.count(i)
+
     return frequencies
